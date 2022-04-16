692a0aca2e618cd99eef685c3c7950e76afcc4b5
guris-ndemir <97963195+guris-ndemir@users.noreply.github.com>
2022-04-16 16:33:38 +0300

    Validation added for new player

---
+++ Services/LightsOutGame.Services.Business/Commands/Validators/PlayerValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using LightsOutGame.Services.Business.Commands.PlayerCreate;
+
+namespace LightsOutGame.Services.Business.Commands.Validators
+{
+    public class PlayerValidator : AbstractValidator<PlayerCreateCommand>
+    {
+        public PlayerValidator()
+        {
+            RuleFor(v => v.Name).NotEmpty().WithMessage("Enter your firstname!");
+            RuleFor(v => v.Surname).NotEmpty().WithMessage("Enter your lastname!");
+            RuleFor(v => v.BoardSize).NotEmpty().WithMessage("Select board size!");
+        }
+    }
+}
